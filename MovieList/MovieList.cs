6ac3f22c67bbce55067e7646f11814345069c330
jonty472 <azttorrs@gmail.com>
2023-07-03 20:39:37 +1200

    restore original file structure

--- MovieList/MovieList.cs
+++ MovieList/MovieList.cs
@@ -1,19 +1,134 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
 
-namespace MovieList
+/*
+ * MVP:
+ * add/remove/clear movies
+ * establish rules around what data can be entered (exceptions)
+ * display movies in a presentable way
+ * have a menu that links to functions
+ */
+namespace MovieList.MovieList1
 {
-    internal class MovieList
+    public class MovieList
     {
+        public List<Movie> ListOfMovies = new List<Movie>();
 
-        public class Movie
+        public string ListName { get; set; }
+        public MovieList(string listname)
         {
-            
+            ListName = listname;
+        }
+
+
+        public void AddMovie(int NumberOfMovies)
+        {
+            for (int i = 0; i < NumberOfMovies; i++)
+            {
+                try
+                {
+
+                    Console.Write("Movie Title: ");
+                    string title = Console.ReadLine();
+
+                    Console.Write("Release Date: ");
+                    string releasedate = Console.ReadLine();
+                    int releasedateyear = Convert.ToInt32(releasedate);
+
+                    Console.Write("Rating: ");
+                    string rating = Console.ReadLine();
+                    int ratingInt = Convert.ToInt32(rating);
+
+                    Movie movie = new Movie(title, releasedateyear, ratingInt, DateTime.Now);
+
+                    Console.WriteLine("Movie added to list\n");
+                    ListOfMovies.Add(movie);
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine($"requries integar {e}");
+                }
+            }
+        }
+
+        public void RemoveMovie(Movie movie)
+        {
+            ListOfMovies.Remove(movie);
+        }
+        public void Clear()
+        {
+            ListOfMovies.Clear();
+        }
+
+
+        public string GetMovieList()
+        {
+            var report = new StringBuilder();
+            report.AppendLine("Title\tReleaseDate\tUserRating\tMovieID");
+            foreach (var movie in ListOfMovies)
+            {
+                report.AppendLine($"{movie.Title}\t{movie.ReleaseDate}\t\t{movie.Rating}\t\t{movie.Name}");
+            }
+            return report.ToString();
         }
     }
 
+    public class Movie
+    {
+        public static int MovieID = 1000;
+        public string Name { get; }
+        public string Title { get; set; }
 
+        private int _releasedate;
+        public int ReleaseDate
+        {
+            get
+            { return _releasedate; }
+            set
+            {
+                if (value < 1885 || value > 2025)
+                {
+                    throw new Exception("Movies release date needs to be within the years 1885 to 2025");
+                }
+                else
+                { _releasedate = value; }
+            }
+        }
+
+        private double _rating;
+        public double Rating
+        {
+            get
+            { return _rating; }
+            set
+            {
+                if (value < 1 || value > 10)
+                {
+                    throw new Exception("Movie Ratings are only between 1-10");
+                }
+                else
+                { _rating = value; }
+            }
+        }
+        public DateTime AddedDate { get; set; }
+
+
+        public Movie(string title, int releaseDate, double rating, DateTime addeddate)
+        {
+            Name = MovieID.ToString();
+            MovieID++;
+            Title = title;
+            ReleaseDate = releaseDate;
+            Rating = rating;
+            AddedDate = addeddate;
+        }
+    }
 }
+
+
--- MovieList/Program.cs
+++ MovieList/Program.cs
@@ -1,21 +1,52 @@
-﻿using System;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
-using System.Threading.Tasks;
+﻿using System.Runtime.CompilerServices;
 
-
-namespace MovieList
+namespace MovieList.MovieList1
 {
     internal class Program
     {
-        static async void Main(string[] args)
+        static void Main(string[] args)
         {
-            using HttpClient client = new HttpClient();
-            HttpResponseMessage response = await client.GetAsync("https://api.themoviedb.org/3/search/movie?api_key=4cc1b68a07fe5ba265950e85ac96cb2c&query=OldBoy&year=2003");
-            response.EnsureSuccessStatusCode();
-            string requestBody = await response.Content.ReadAsStringAsync();
-            Console.WriteLine(requestBody);
+
+
+
+            MovieList mylist = new MovieList("Movie List");
+
+            bool showMenu = true;
+            while (showMenu)
+            {
+                showMenu = MainMenu(mylist);
+            }
+
+        }
+
+        private static bool MainMenu(MovieList list)
+        {
+            Console.Clear();
+            Console.WriteLine(list.GetMovieList());
+            Console.WriteLine("Choose an option:");
+            Console.WriteLine("1) Add movie");
+            Console.WriteLine("2) Remove movie");
+            Console.WriteLine("3) Clear movie list");
+            Console.WriteLine("4) Exit");
+            Console.Write("\r\nSelect an option: ");
+
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    list.AddMovie(1);
+                    return true;
+                case "2":
+                    ;
+                    return true;
+                case "3":
+                    list.Clear();
+                    return true;
+                case "4":
+                    return false;
+
+            }
+            return true;
         }
     }
 }
